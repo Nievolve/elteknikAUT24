f3c976f6314e808320dcbba0081ad936669aeea2
Andreas <131877497+Nievolve@users.noreply.github.com>
2024-09-19 15:45:05 +0200

    Update fi.py

--- Elteknik/fi.py
+++ Elteknik/fi.py
@@ -78,7 +78,7 @@ print(f"a) Xl = {Xl:.2f} Omh")
 print(f"b) Z = {Z:.2f} Ohm")
 print(f"c) Fasförskjutningvinkeln är : {tanfi:.2f} grader")
 print(f"d) Strömmen om spänningen är 230V är : {I:.2f} A")
-math.factorial
+
 #94
 print("94")
 Xl=90
@@ -98,7 +98,7 @@ print(f"Fasförskjutningsvinkeln = {math.degrees(fiRad):.2f} grader")
 
 #95
 print("95")
-L = "?" #Indukdans
+L = "?" #Indukdans 
 Xl = 42 #Reaktans
 f = 200 #Hz
 # Xl= 2*Pi*f*L
@@ -111,3 +111,84 @@ R = 17
 Xl = 12
 Z = "?" #Impedansen
 print(f"Impedansen är : {math.sqrt(R**2+Xl**2):.4g}")
+
+
+#97
+print("97")
+
+# 1. Löser ut Xc
+# 2. Löser ut Uc genom att ta I genom Xc
+# Beräkna U? 
+C = 2*10**-6 #F
+print(C)
+f = 50
+I = 0.015
+Xc = 1/(2*math.pi*f*C)
+print(f"Reaktansen är : {Xc:.2f}")
+
+Uc = I * Xc
+
+print(f"Uc = {Uc:.2f} V")
+
+#98
+print("98")
+R = 20
+Xc = 40
+Z = math.sqrt(R**2+Xc**2)
+f = 50
+print(f"Z är {Z:.2f} Ohm")
+print(f"Kondensatorn kapacitet är {1/(2*math.pi*f*Xc)*10**6:.2f} microFarat")
+
+#99
+print("99")
+Xc = "?"
+C = 220*10**-6
+f = 50
+print(f"Reaktansen(Xc) är {1/(2*math.pi*f*C):.2f} Ohm")
+
+#111
+print("111")
+R = 22
+Xc = 12
+U = 48
+f = 50
+I = "?"
+Z = math.sqrt(R**2+Xc**2)
+I = U/Z
+print(f"Strömmen beräknades med U/Z och är i kretsen {I:.2g} A")
+
+#112
+print("112")
+R = 48
+Xc = 12
+print(f"Impedansen beräknades med Z²=R²+Xc² och är {math.sqrt(R**2+Xc**2):.2f}")
+
+#113
+print("113")
+U = 127
+I = 1.7
+Ur = 87
+Uc = math.sqrt(U**2-Ur**2)
+Ux = Uc/I
+print(f"Uc = {Uc:.2f} V")
+print(f"Ux = {Ux:.2f} V")
+
+
+#114
+print("114")
+R = 20
+C = 8*10**-6
+f = 50
+U = 20
+f = 100
+Xc = 1/(2*math.pi*f*C)
+Z = math.sqrt(Xc**2+R**2)
+I = U/Z
+S = U * I
+P = U * I * math.cos(R/Z)
+cosfi = P/S
+Nfasvinkelfi = math.degrees(math.acos(R/Z))
+print(f"Z = {Z:.2f} Ohm")
+print(f"Strömmen I är {I*10**3:.2f} mA")
+print(f"Effektfaktorn är {cosfi:.2f} ")
+print(f"Fi = {Nfasvinkelfi:.2f} grader")
