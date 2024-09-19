b316443717a2befb9269c99322e71e07104f5074
Andreas <131877497+Nievolve@users.noreply.github.com>
2024-09-19 11:20:50 +0200

    update

--- Elteknik/fi.py
+++ Elteknik/fi.py
@@ -14,11 +14,12 @@ print(f"U = {80 / math.cos(math.radians(30))}")
 # Voltmeter visar som kopplas in över hela kretsen?
 Ur = 5
 Ul = 12
+# U²=Ur²+Ul²
 print("83")
 print(f"Spänningen är {math.sqrt(Ur**2 + Ul**2):.2f}")
 
 #84
-# Ul = ?
+# Ul²=U²-Ur²
 U = 28
 Ur = 19
 print("84")
@@ -30,7 +31,6 @@ omhmeter = 13.3
 U = 12
 f = 50
 I = 0.6
-
 #Ur=R*I
 Ur = omhmeter*I
 #Ul²=U²-Ur²
@@ -40,3 +40,25 @@ print("85")
 print(f"Spänningen över Ur = {Ur:.2f}")
 print(f"Spänningen över Ul = {Ul:.2f}")
 
+
+#90
+Ur = 10
+Uc = 20
+U = math.sqrt(Ur**2+Uc**2)
+fi = math.degrees(math.acos(Ur/U))
+print("90")
+print(f"Spänningen är : {U:.2f}")
+print(f"Viknel fi är : {fi:.2f}")
+
+#91
+U = 100
+fi = 40 #grader
+Ur = "?"
+Uc = "?"
+# Ur= Cos(40)* U
+Ur = math.cos(math.radians(40))*U
+print("91")
+print(f"Ur = {Ur:.2f}")
+#Uc²=U²-Ur²
+Uc = math.sqrt(U**2-Ur**2)
+print(f"UC = {Uc:.2f}")
