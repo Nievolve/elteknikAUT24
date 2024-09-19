3e95a3e25346a5f918dabed7d20ffdf2de52806c
Andreas <131877497+Nievolve@users.noreply.github.com>
2024-09-19 13:30:58 +0200

    Update fi.py

--- Elteknik/fi.py
+++ Elteknik/fi.py
@@ -66,9 +66,9 @@ print(f"UC = {Uc:.2f}")
 
 #93
 print("93")
-R = 20
-L = 0.2
-f = 50
+R = 20 #Resistans
+L = 0.2 #Henry, Indukdans
+f = 50 #Hz, 
 Xl = 2*math.pi*f*L
 Z = math.sqrt(R**2+Xl**2)
 tanfi = math.degrees(math.atan(Xl/R))
@@ -78,7 +78,7 @@ print(f"a) Xl = {Xl:.2f} Omh")
 print(f"b) Z = {Z:.2f} Ohm")
 print(f"c) Fasförskjutningvinkeln är : {tanfi:.2f} grader")
 print(f"d) Strömmen om spänningen är 230V är : {I:.2f} A")
-
+math.factorial
 #94
 print("94")
 Xl=90
@@ -95,3 +95,19 @@ print(f"Z = {Z:.3g}")
 R = math.sqrt(Z**2-Xl**2)
 print(f"REsistansen (R) = {R:.3g}")
 print(f"Fasförskjutningsvinkeln = {math.degrees(fiRad):.2f} grader")
+
+#95
+print("95")
+L = "?" #Indukdans
+Xl = 42 #Reaktans
+f = 200 #Hz
+# Xl= 2*Pi*f*L
+L = Xl/(2*math.pi*f)
+print(f"a) Induktansen är : {L:.3g} H. Kan också skrivas som {L*10**-3:.3g}")
+
+#96
+print("96")
+R = 17
+Xl = 12
+Z = "?" #Impedansen
+print(f"Impedansen är : {math.sqrt(R**2+Xl**2):.4g}")
